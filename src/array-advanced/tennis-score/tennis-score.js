75988d54a19c858beafb25ecc3ee87033db6eb1e
Baris G <barisg@pm.me>
2023-10-07 19:57:14 +0200

    fini tennis-score mais pas le debug

--- src/array-advanced/tennis-score/tennis-score.js
+++ src/array-advanced/tennis-score/tennis-score.js
@@ -29,12 +29,87 @@ Add you own tests.
 */
 
 // TODO add your code here
-
+function getScore(array) {
+  if (array === null || array.isArray) {
+    throw TypeError("L'argument n'a pas le bon type");
+  }
+  if (array.every((element) => element === null || isNaN(element))) {
+    throw TypeError("Les éléments ne sont pas tous des entiers");
+  }
+  if (!array.every((element) => element === 1 || element === 2)) {
+    throw RangeError("Il faut absolument un tableau avec des 1 ou 2");
+  }
+  let p1, p2;
+  if (array.length < 7) {
+    p1 = array.filter((x) => x === 1).length;
+    p2 = array.filter((x) => x === 2).length;
+    switch (p1) {
+      case 0:
+        p1 = "love";
+        break;
+      case 1:
+        p1 = 15;
+        break;
+      case 2:
+        p1 = 30;
+        break;
+      case 3:
+        p1 = 40;
+        break;
+      default:
+        break;
+    }
+    switch (p2) {
+      case 0:
+        p2 = "love";
+        break;
+      case 1:
+        p2 = 15;
+        break;
+      case 2:
+        p2 = 30;
+        break;
+      case 3:
+        p2 = 40;
+        break;
+      default:
+        break;
+    }
+    if (p1 === p2) {
+      return "deuce";
+    }
+    return `${p1}-${p2}`;
+  } else {
+    const avance = array.slice(-1);
+    if (avance[0] === 1) {
+      return "ad in";
+    } else {
+      return "ad out";
+    }
+  }
+}
 // Begin of tests
 const assert = require("assert");
+const { get } = require("http");
 
 assert.strictEqual(typeof getScore, "function");
 assert.strictEqual(getScore.length, 1);
-// TODO add your tests:
 
+// TODO add your tests:
+assert.deepStrictEqual(getScore([1, 1, 1]), "40-love");
+assert.deepStrictEqual(getScore([2, 1, 2, 2]), "15-40");
+assert.deepStrictEqual(getScore([1, 2, 1, 2, 1, 2]), "deuce");
+assert.deepStrictEqual(getScore([1, 1, 1, 2, 2, 2, 1]), "ad in");
+assert.deepStrictEqual(getScore([1, 1, 1, 2, 2, 2, 2]), "ad out");
+assert.throws(() => {
+  getScore([1, 2, 3, 1, 2, 1]);
+}, RangeError);
+assert.throws(() => {
+  getScore([1, null, 3, 1, 2, 1]);
+}, TypeError);
+assert.throws(() => {
+  getScore(null);
+}, TypeError);
 // End of tests
+
+console.log("Bravo!");
